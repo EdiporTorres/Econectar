5da065bca344e23fca639c8e6b1f8c40d48ed06b
Édipo Reis <ediporeiss@gmail.com>
2024-08-07 14:57:59 -0300

    Footer

--- Econectar/src/Components/Footer/Footer.tsx
+++ Econectar/src/Components/Footer/Footer.tsx
@@ -10,9 +10,10 @@ function Footer() {
         <div className="flex justify-center bg-indigo-900 text-white">
           <div className="container flex flex-col items-center py-4">
             <p className='text-xl font-bold'>E-Conectar - Onde conexões geram soluções  | Copyright: </p>
-            <p className='text-lg'>Acesse nossas redes sociais</p>
             <div className='hover:underline'>Contatos</div>
             <div className='hover:underline'>SAC</div>
+            <p className='text-lg'>Acesse nossas redes sociais</p>
+
             <div className='flex gap-2'>
               <LinkedinLogo  size={48} weight='bold' />
               <InstagramLogo   size={48} weight='bold' />
