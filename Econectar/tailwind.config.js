394a05cdf64e512ac1e5a5c98aa9edbf61140984
Daniel Lucas <daniellucaspc@hotmail.com>
2024-08-21 14:36:03 -0300

    First commit

--- Econectar/tailwind.config.js
+++ Econectar/tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'azul-padrao': '#4158A6',
+    },
   },
   plugins: [],
+}
 }
