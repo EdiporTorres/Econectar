3d062fc284b3d04faf4bf9c965035e6770a70665
eduardowada <156962925+eduardowada@users.noreply.github.com>
2024-08-22 22:11:54 -0300

    atualizando cards

--- Econectar/src/Components/Servico/CardServico.tsx
+++ Econectar/src/Components/Servico/CardServico.tsx
@@ -1,27 +1,25 @@
 import { Link } from 'react-router-dom'
 import Servico from '../../Models/Servico'
-
+import './CardServico.css'; // Importa o CSS personalizado
 
 interface CardServicoProps {
   serv: Servico
 }
 
-function CardServico({serv}: CardServicoProps) {
+function CardServico({ serv }: CardServicoProps) {
   return (
-    <div className='border-2 border-blue-500 border-solid flex flex-col rounded-2xl overflow-hidden justify-between'>
-      <div>
-        <div className='p-4 bg-white'>
-          <h4 className='text-lg font-semibold uppercase'>{serv.nomeServico}</h4>
-          <p>{serv.descricao}</p>
-          <p>Categoria: {serv.Categoria?.nomeCategoria}</p>
-          <p>Valor: {serv.valor.toFixed(2)}</p>
-        </div>
+    <div className='card-servico'>
+      <div className='card-servico-content'>
+        <h4 className='text-lg font-semibold uppercase'>{serv.nomeServico}</h4>
+        <p>{serv.descricao}</p>
+        <p>Categoria: {serv.Categoria?.nomeCategoria}</p>
+        <p>Valor: {serv.valor.toFixed(2)}</p>
       </div>
-      <div className="flex">
-      <Link to={`/editarServico/${serv.id}`} className='w-full text-white bg-orange-400  hover:bg-orange-500 flex items-center justify-center py-2'>
+      <div className='card-servico-footer'>
+        <Link to={`/editarServico/${serv.id}`} className='edit-button'>
           <button>Editar</button>
         </Link>
-        <Link to={`/deletarServico/${serv.id}`} className='text-white bg-red-500 hover:bg-red-700 w-full flex items-center justify-center'>
+        <Link to={`/deletarServico/${serv.id}`} className='delete-button'>
           <button>Deletar</button>
         </Link>
       </div>
